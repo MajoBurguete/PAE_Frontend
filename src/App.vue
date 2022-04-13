893ec51ff3c40765e97c263977c6e7b6b475484e
Majo Burguete <marijo.mjbe@gmail.com>
2022-04-12 22:52:51 -0500

    Asset were added (img and fonts)

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <header>
     <div class="wrapper">
-      <nav>
+      <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
-      </nav>
+      </nav> -->
     </div>
   </header>
 
@@ -15,5 +15,78 @@ import { RouterLink, RouterView } from "vue-router";
 </template>
 
 <style>
+
+@font-face {
+  font-family: "Catamaran";
+  src: url('src/assets/fonts/Catamaran-Bold.ttf');
+  font-weight: bold;
+}
+
+@font-face {
+  font-family: "Catamaran";
+  src: url('src/assets/fonts/Catamaran-Medium.ttf');
+  font-weight: medium;
+}
+
+@font-face {
+  font-family: "Catamaran";
+  src: url('src/assets/fonts/Catamaran-Regular.ttf');
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: "Catamaran";
+  src: url('src/assets/fonts/Catamaran-Light.ttf');
+  font-weight: lighter;
+}
+
+@font-face {
+  font-family: "Montserrat";
+  src: url('src/assets/fonts/Montserrat-Bold.ttf');
+  font-weight: bolder;
+}
+
+@font-face {
+  font-family: "Montserrat";
+  src: url('src/assets/fonts/Montserrat-SemiBold.ttf');
+  font-weight: bold;
+}
+
+@font-face {
+  font-family: "Montserrat";
+  src: url('src/assets/fonts/Montserrat-Medium.ttf');
+  font-weight: medium;
+}
+
+@font-face {
+  font-family: "Montserrat";
+  src: url('src/assets/fonts/Montserrat-Regular.ttf');
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: "Ubuntu";
+  src: url('src/assets/fonts/Ubuntu-Italic.ttf');
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: "Ubuntu";
+  src: url('src/assets/fonts/Ubuntu-LightItalic.ttf');
+  font-weight: lighter;
+}
+
+
+body {
+    display: inline-block;
+    padding: 0;
+    margin: 0;
+}
+
+html {
+    display: inline-block;
+    padding: 0;
+    margin: 0; 
+}
 
 </style>
