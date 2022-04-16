76e2332ac10582ab57c47cf09c6cae7233f2a885
Majo Burguete <marijo.mjbe@gmail.com>
2022-04-16 01:14:22 -0500

    Login base #4

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <body>
+        <div class="flexContainer">
+            <div class="section-login" id="section-login">
+                <div class="container-signup" id="container-signup">
+                    <h1> ¡Bienvenidx! </h1>
+                    <button class="signup" id="signup" type="button"> Regístrate </button>
+                    <h3> ¿Aun no te has registrado? </h3>
+                </div>
+            </div>
+
+            <div class="section-signup" id="section-signup">
+                <div class="login-form" id="login-form">
+                    <img src="src/assets/img/PAE_Logo_con_nombre.png" alt="PAELogoNotFound">
+                    <div class="form">
+                        <h3 class="login-h3">Correo</h3>
+                        <input type="text">
+                        <h3 class="login-h3">Contraseña</h3>
+                        <input type="text">
+                        <h3 class="login-question-h3">¿Olvidaste tu contraseña?</h3>
+                    </div>
+                    <button id="signin-button">Iniciar Sesión</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</template>
