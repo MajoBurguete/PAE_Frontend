3e1f3e4662cde652f718143012f5d019b60569b9
Majo Burguete <marijo.mjbe@gmail.com>
2022-04-26 10:27:06 -0500

    Basic layout #8

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LoginSignupView from "@/views/LoginSignupView.vue";
+import LoginSignupView from "@/views/SessionQuestionView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
