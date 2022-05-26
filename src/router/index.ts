3bda48070665ae9474971139f08723cae6c3fe3f
MajoBurguete <A01730344@itesm.mx>
2022-05-26 16:32:49 -0500

    Added paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -35,6 +35,11 @@ const router = createRouter({
       component: () => import('../views/StudentConfigView.vue')
     },
     {
+      path: "/tutor-settings",
+      name: "tutorSettings",
+      component: () => import('../views/TutorConfigView.vue')
+    },
+    {
       path: "/question",
       name: "sessionQuestion",
       component: () => import('../views/SessionQuestionView.vue')
