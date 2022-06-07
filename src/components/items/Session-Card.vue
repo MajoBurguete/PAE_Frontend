c1aecd359bb33495571655c9d0027b8c811426c8
AaronCG01 <A01730451@itesm.mx>
2022-06-07 18:44:20 -0500

    CancelBtn default value as true in Session-Card.vue

--- src/components/items/Session-Card.vue
+++ src/components/items/Session-Card.vue
@@ -51,7 +51,7 @@ export default defineComponent({
         },
         cancelBtn: {
             type: Boolean,
-            default: false
+            default: true
         }
     },
     data(){
